a1dc1650227727967339e472f9d64f0ad6ac7966
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-08-29 13:24:54 +0100

    chore: use most up to date version of roam

--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 (package! exec-path-from-shell)
 (package! lsp-origami)
 (package! catppuccin-theme)
+(package! org-roam-ui)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
@@ -51,3 +52,4 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+(unpin! org-roam)
